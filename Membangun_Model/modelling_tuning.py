94507065bd179024f50006034c4e6a837e01c307
ItsNudle <ghifari.fikri.yulistia@gmail.com>
2025-06-16 18:54:34 +0700

    Modelling dan Tuning

--- Membangun_Model/modelling_tuning.py
+++ Membangun_Model/modelling_tuning.py
@@ -1,44 +1,57 @@
 import os
-from dagshub import dagshub
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from scipy.stats import uniform
+import joblib
 import mlflow
 import mlflow.sklearn
-import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import joblib
+from mlflow.models.signature import infer_signature
+from dagshub import dagshub_logger
 
 os.environ["MLFLOW_TRACKING_USERNAME"] = "ghifari.fikri.yulistia"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = "28a2bed8301cd660e33707a009cb925162d47426"
+
 dagshub.init(repo_owner='ghifari.fikri.yulistia', repo_name='SMSML_Ghifari-Fikri-Yulistia', mlflow=True)
 
-X = pd.read_csv("tfidf.csv")
-y = pd.read_csv("labels.csv")["label"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X = pd.read_csv("Membangun_Model/spam_ham_emails_preprocessing/tfidf.csv")
+y = pd.read_csv("Membangun_Model/spam_ham_emails_preprocessing/labels.csv")["label"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-params = {
-    'C': [0.01, 0.1, 1, 10],
+model = LogisticRegression(max_iter=100)
+param_dist = {
+    'C': uniform(0.1, 1),
     'solver': ['liblinear', 'lbfgs']
 }
-model = LogisticRegression(max_iter=200)
-clf = GridSearchCV(model, param_grid=params, cv=3)
-clf.fit(X_train, y_train)
+search = RandomizedSearchCV(model, param_distributions=param_dist, n_iter=4, cv=3, random_state=42, verbose=1)
+search.fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
+y_pred = search.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 prec = precision_score(y_test, y_pred)
 rec = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 
 with mlflow.start_run():
-    mlflow.log_param("best_C", clf.best_params_['C'])
-    mlflow.log_param("best_solver", clf.best_params_['solver'])
+    mlflow.log_param("best_C", search.best_params_['C'])
+    mlflow.log_param("best_solver", search.best_params_['solver'])
+
     mlflow.log_metric("accuracy", acc)
     mlflow.log_metric("precision", prec)
     mlflow.log_metric("recall", rec)
     mlflow.log_metric("f1_score", f1)
 
-    joblib.dump(clf.best_estimator_, "best_model.pkl")
-    mlflow.sklearn.log_model(clf.best_estimator_, "model")
+    joblib.dump(search.best_estimator_, "best_model.pkl")
 
-print("✅ Training selesai dan dilogging ke DagsHub!")
+    signature = infer_signature(X_train, search.predict(X_train))
+    input_example = X_train.iloc[:1]
+
+    mlflow.sklearn.log_model(
+        search.best_estimator_,
+        "model",
+        signature=signature,
+        input_example=input_example
+    )
+
+print("✅ Model berhasil dituning dan dicatat di DagsHub MLflow.")
