2383049b5c0757b8b59fb3004d3ac361f82576c4
ItsNudle <115250029+ItsNudle@users.noreply.github.com>
2025-06-19 12:03:52 +0700

    inference.py File

--- Monitoring dan Logging/7.inference.py
+++ Monitoring dan Logging/7.inference.py
@@ -1,10 +1,10 @@
 import requests
 import pandas as pd
 
-data = {
-  "columns": ["feature1", "feature2", ...],
-  "data": [[...]]
-}
-
-res = requests.post("http://localhost:5000/invocations", json=data)
-print(res.json())
+data = pd.DataFrame([[0.1, 0.5, 0.7]], columns=["feat1", "feat2", "feat3"])
+response = requests.post(
+    url="http://localhost:5000/invocations",
+    headers={"Content-Type": "application/json"},
+    json={"columns": list(data.columns), "data": data.values.tolist()}
+)
+print(response.json())
