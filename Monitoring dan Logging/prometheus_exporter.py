95ad3da42bef7465b24006286b24884624b41625
ItsNudle <ghifari.fikri.yulistia@gmail.com>
2025-06-19 22:13:49 +0700

    Prometheus Exporter

--- Monitoring dan Logging/prometheus_exporter.py
+++ Monitoring dan Logging/prometheus_exporter.py
@@ -1,23 +1,20 @@
 from prometheus_client import start_http_server, Summary, Counter, Gauge
 import requests
 import time
-import random
 import json
 
 INFERENCE_TIME = Summary('inference_processing_seconds', 'Time spent processing inference')
 INFERENCE_COUNTER = Counter('inference_requests_total', 'Total number of inference requests')
 MODEL_ACCURACY = Gauge('model_accuracy_score', 'Model accuracy score')
 
-
 MODEL_ACCURACY.set(0.92)
 
 @INFERENCE_TIME.time()
 def infer():
     INFERENCE_COUNTER.inc()
-    
-    payload = {
-        "inputs": [[0.0] * 3000]
-    }
+
+    with open("serving_input_example.json", "r") as f:
+        payload = json.load(f)
 
     response = requests.post(
         "http://localhost:5000/invocations",
@@ -33,7 +30,7 @@ def infer():
 if __name__ == "__main__":
     start_http_server(8001)
     print("🚀 Prometheus exporter running at http://localhost:8001/metrics")
-    
+
     while True:
         infer()
         time.sleep(5)
