b249d9897eb7b0cf219301a4cd01e1dee0a60b27
ItsNudle <ghifari.fikri.yulistia@gmail.com>
2025-06-19 22:04:54 +0700

    Prometheus Exporter

--- Monitoring dan Logging/prometheus_exporter.py
+++ Monitoring dan Logging/prometheus_exporter.py
@@ -4,21 +4,19 @@ import time
 import random
 import json
 
-# Metrik Prometheus
 INFERENCE_TIME = Summary('inference_processing_seconds', 'Time spent processing inference')
 INFERENCE_COUNTER = Counter('inference_requests_total', 'Total number of inference requests')
 MODEL_ACCURACY = Gauge('model_accuracy_score', 'Model accuracy score')
 
-# Simulasi atau bisa diganti nilai sebenarnya
-MODEL_ACCURACY.set(0.92)  # contoh akurasi
 
-# Fungsi inference
+MODEL_ACCURACY.set(0.92)
+
 @INFERENCE_TIME.time()
 def infer():
     INFERENCE_COUNTER.inc()
     
     payload = {
-        "inputs": [[0.0] * 3000]  # Contoh input dummy, sesuaikan jumlah fitur model kamu
+        "inputs": [[0.0] * 3000]
     }
 
     response = requests.post(
@@ -38,4 +36,4 @@ if __name__ == "__main__":
     
     while True:
         infer()
-        time.sleep(5)  # jeda antar request
+        time.sleep(5)
